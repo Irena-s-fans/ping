02e2f1dd7cf86ea8326c0a5004bd377486b37695
Mark <samojlenko.wot@gmail.com>
2023-04-29 22:53:12 +0300

    hotfix add

--- resources/views/project_add.blade.php
+++ resources/views/project_add.blade.php
@@ -14,7 +14,7 @@
     <h2 class="admin__title">
         Добавить проект
     </h2>
-    <form class="form admin__form" data-url="/admin/projects/add">
+    <form class="form admin__form" data-url="/projects/add">
         <div class="form__block">
             <input name="lang" type="checkbox" id="lang" hidden>
             <label class="form__checkbox-label" for="lang">
